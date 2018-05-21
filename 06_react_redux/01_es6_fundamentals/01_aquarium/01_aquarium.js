6fdab9510ca988c1ae4f712e99b3e67daffa6611
Jiao LI <isabellejiao@hotmail.com>
2018-05-21 18:14:23 +0200

    updated work

--- 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
+++ 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
@@ -1,6 +1,61 @@
-// Insert code here
+class Fish {
+  constructor(name, sex, vegan) {
+    this.name = name;
+    this.sex = sex;
+    this.vegan = vegan;
+  }
 
+  eat(aquarium) {
+    if (this.vegan) {
+      if (aquarium.algaes.length > 0) {
+      aquarium.algaes.pop();
+      }
+    } else {
+      if (aquarium.fishes.length > 0) {
+      aquarium.fishes.pop();
+      }
+    }
+  }
+}
 
+class Algae {
+  constructor(size) {
+    this.size = size;
+  }
+}
+
+class Aquarium {
+  constructor(fishes = [], algaes = []) {
+    this.fishes = fishes;
+    this.algaes = algaes;
+  }
+
+  addFish(fish) {
+    this.fishes.push(fish);
+  }
+
+  addAlgae(algae) {
+    this.algaes.push(algae);
+  }
+
+  passTime() {
+    console.log(this.algaes.length);
+
+    this.fishes.forEach((fish) => {
+      console.log(fish.name);
+      console.log(fish.sex);
+    });
+    this.fishes.forEach((fish) => {
+      fish.eat(this);
+    });
+  }
+
+  endTurn() {
+    while(this.fishes.length >1) {
+      this.passTime();
+    }
+  }
+}
 
 module.exports = {
   aquarium: Aquarium,
